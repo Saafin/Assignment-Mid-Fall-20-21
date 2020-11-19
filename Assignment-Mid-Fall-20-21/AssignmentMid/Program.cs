d2043cbccf43dc8f0df7a2bbdb8b1b9828ce54f4
Saafin <72722412+Saafin@users.noreply.github.com>
2020-11-19 22:47:48 +0600

    Mid assignment done
    Mid assignment done

---
+++ Assignment-Mid-Fall-20-21/AssignmentMid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentMid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Department Info input***************");
+            Console.WriteLine("Enter Department name: ");
+            String dn = Console.ReadLine();
+            Department d = new Department(dn);
+            Course oop2 = new Course("Course1", "Courseid");
+            Course oop1= new Course("Course1", "Courseid");
+            d.AddCourse(oop2);
+            d.AddCourse(oop1);
+            Section A = new Section("A");
+            oop1.AddSection(A);
+            oop2.AddSection(A);
+            Faculty fac = new Faculty("Safin", "1010101010", 9.5f);
+            fac.AddSection(A);
+            A.AddTeacher(fac);
+            Console.WriteLine("Department name: ");
+            d.ShowDInfo();
+            oop1.ShowSectionFaculty();
+            Console.WriteLine("\nFaculty object");
+            fac.ShowSectionDetails();
+            Console.ReadLine();
+        }
+   
+    }
+}
---
+++ Assignment-Mid-Fall-20-21/AssignmentMid/Section.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentMid
+{
+    class Section
+    {
+        private String name;
+        Faculty FacultyName;
+
+        public void getSname(String n)
+        {
+            name = n;
+        }
+        public String setSname()
+        {
+            return name;
+        }
+        public Section(string sectionName)
+        {
+            getSname(sectionName);
+        }
+
+        public void AddTeacher(Faculty teacherName)
+        {
+            if (teacherName.Counttime < 21)
+            {
+                FacultyName = teacherName;
+            }
+            else
+            {
+                Console.WriteLine("course can't add");
+            }
+
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Section:" + name);
+            FacultyName.ShowFInfo();
+        }
+    }
+}
